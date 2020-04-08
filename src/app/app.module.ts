6ed5679ed6ffc93e2c8b58c01ac4f806a6e3c30b
Anusha Manda <amanda@unomaha.edu>
2020-04-08 16:50:29 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table'  
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    MatTableModule, BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
